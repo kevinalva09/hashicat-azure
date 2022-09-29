85890d7d25910451d699e1842358f40f933e80ed
Kevin Alva <kevinalva09@gmail.com>
2022-09-29 17:08:48 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,7 +1,6 @@
-/*module "network" {
+module "network" {
   source  = "app.terraform.io/myorg-kevin/network/azurerm"
   version = "3.5.0"
   resource_group_name = "${var.prefix}-workshop"
   # insert required variables here
 }
-*/
